02fa6a29071d293084795a28a56879a6660b3e98
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-20 11:25:46 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Vdd
+admin.site.register(Vdd)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Vdd(models.Model):
+    'Generated Model'
+    ccnc = models.BigIntegerField()
+    utri = models.BigIntegerField()
