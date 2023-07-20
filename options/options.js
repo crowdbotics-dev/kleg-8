7a0b8b057758480000b2eedde1f9a12a6fc22218
Crowdbotics <engineering@crowdbotics.com>
2023-07-20 10:18:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kleg-8",
+  url: "https://kleg-8.botics.co",
+  api: "https://kleg-8.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
